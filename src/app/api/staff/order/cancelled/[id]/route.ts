ac95fd294e6667fe4132480b73347842f3892940
WhoaWicked <9uy0232@gmail.com>
2026-02-23 00:52:29 +0700

    feat: update import paths for authOptions in order API routes

--- src/app/api/staff/order/cancelled/[id]/route.ts
+++ src/app/api/staff/order/cancelled/[id]/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 import { getServerSession } from 'next-auth';
-import { authOptions } from "../../auth/[...nextauth]/route";
+// import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "../../../../auth/[...nextauth]/route";
+
 
 export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     try {
