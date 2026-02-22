9671d4a78ef8318dc0b9830c6447a56a482d46c1
WhoaWicked <9uy0232@gmail.com>
2026-02-23 01:32:07 +0700

    feat: enhance login page layout with background image and improved form design

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { PiShoppingBagLight, PiUser } from "react-icons/pi";
+import { PiScissorsLight, PiShoppingBagLight, PiUser } from "react-icons/pi";
 import { SlLock } from "react-icons/sl";
 import Swal, { SweetAlertIcon } from "sweetalert2";
 import axios from "axios";
@@ -61,7 +61,7 @@ export default function Login() {
 
     return (
         <div id="login-page">
-            <div className="h-screen grid grid-cols-[2fr_1fr]">
+            {/* <div className="h-screen grid grid-cols-[2fr_1fr]">
                 <div className="bg-gray-300"></div>
                 <div className="flex items-center justify-center">
                     <div className="px-8 flex flex-col justify-center w-full">
@@ -105,6 +105,59 @@ export default function Login() {
 
                                     <div className="bg-black  duration-300 shadow-md cursor-pointer hover:shadow-lg hover:scale-105">
                                         <button type="submit" className="p-3 text-center text-sm w-full cursor-pointer  font-light text-white tracking-wide">เข้าสู่ระบบ</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+            <div className="relative h-screen flex items-center justify-center overflow-hidden">
+                <div className="absolute grayscale-100 inset-0 bg-[url('https://images.unsplash.com/photo-1751738567808-6affa516fedc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-center bg-cover"
+                >
+                </div>
+                <div className="relative z-10 bg-white border border-gray-300 p-8 rounded-lg shadow-lg w-120">
+                    <div className=" flex flex-col justify-center w-full">
+                        <div className="mb-10">
+                            <div className="mb-5">
+                                <PiScissorsLight className="text-gray-700 mx-auto" size={50} />
+                            </div>
+                            <div className="flex items-center justify-center gap-x-4 mb-5">
+                                <h1 className="text-gray-700 text-4xl font-light">Clothing Store</h1>
+                                <div className="mt-2 w-15 h-0.75 bg-gray-600"></div>
+                            </div>
+                            <div>
+                                <h2 className="text-gray-600 text-2xl font-light text-center tracking-wide">ยินดีต้อนรับกลับมา !</h2>
+                            </div>
+                        </div>
+                        <div>
+                            <form action="" onSubmit={handleSubmit}>
+                                <div className="space-y-6">
+                                    <div className="flex flex-col gap-y-2">
+                                        <label htmlFor="email" className="text-sm tracking-wide text-gray-600 font-light">
+                                            บัญชีผู้ใช้
+                                        </label>
+                                        <div className="flex items-center gap-x-4 border border-gray-300  px-4 py-2.5 duration-300 focus-within:ring-1 focus-within:ring-gray-500 shadow-md">
+                                            <PiUser className="text-gray-400" size={20} />
+                                            <input onChange={(e) => setEmail(e.target.value)} type="text" className="font-light w-full tracking-wide text-gray-600 focus:outline-none" placeholder="เช่น clothing@gmail.com" autoFocus />
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-col gap-y-2">
+                                        <label htmlFor="password" className="text-sm tracking-wide text-gray-600 font-light">
+                                            รหัสผ่าน
+                                        </label>
+                                        <div className="flex items-center gap-x-4 border border-gray-300  px-4 py-2.5 duration-300 focus-within:ring-1 focus-within:ring-gray-500 shadow-md">
+                                            <SlLock className="text-gray-400" size={20} />
+                                            <input onChange={(e) => setPassword(e.target.value)} type="password" className="font-light w-full tracking-wide text-gray-600 focus:outline-none" placeholder="รหัสผ่าน" />
+                                        </div>
+                                        <div className="flex justify-between items-center">
+                                            <p className="w-fit cursor-pointer text-sm font-light text-blue-600">ลืมรหัสผ่าน</p>
+                                            <p className="w-fit cursor-pointer text-sm font-light text-gray-700">สมัครสมาชิก</p>
+                                        </div>
+                                    </div>
+
+                                    <div className="bg-black  duration-300 shadow-md cursor-pointer hover:shadow-lg hover:scale-105 active:scale-95">
+                                        <button type="submit" className="p-3 text-center text-sm w-full cursor-pointer  font-light text-white tracking-wide ">เข้าสู่ระบบ</button>
                                     </div>
                                 </div>
                             </form>
