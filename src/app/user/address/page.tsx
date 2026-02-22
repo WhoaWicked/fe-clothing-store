984e2283c165922554b8a41c44f693fceb1379d1
WhoaWicked <9uy0232@gmail.com>
2026-02-23 02:09:25 +0700

    feat: dynamically import AddressList component for improved performance

--- src/app/user/address/page.tsx
+++ src/app/user/address/page.tsx
@@ -1,6 +1,10 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { AddressList } from '@/components/user/Address';
+import dynamic from 'next/dynamic';
+const AddressList = dynamic(
+    () => import('@/components/user/Address').then((mod) => mod.AddressList),
+    { ssr: false } // <--- พระเอกของเราคือบรรทัดนี้ครับ
+);
 
 export default function Page() {
     return (
